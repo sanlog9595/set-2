a420a0d7ebe2cfd0b26b902dc4d7845c68319e4e
sanlog9595 <49307730+sanlog9595@users.noreply.github.com>
2019-05-13 15:31:41 +0530

    Many.java

---
+++ Many.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+Class Many
+{
+   public static void main(String arg[])
+	
+   {
+	
+       long n,p,r=1;
+	
+       Scanner sc=new Scanner(System.in);
+ 
+      //System.out.println("enter number");
+	  
+       n=sc.nextLong();
+ 
+       //System.out.println("enter power");
+	  
+       p=sc.nextLong();
+ 
+           for(int i=1;i<=p;i++)
+	       { 
+                   r=r *n;
+	       }
+ 
+    	  System.out.println(r);
+ 
+    }
+ 
+}
