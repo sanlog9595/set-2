3bd376eec231720f20078c583343b900f13bcebd
sanlog9595 <49307730+sanlog9595@users.noreply.github.com>
2019-05-13 15:41:40 +0530

    prim.java

---
+++ prim.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prim
+{
+public static void main(String[] args) {
+int num ;
+boolean flag = false;
+System.out.println("");
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+for(int i = 2; i <= num/2; ++i)
+{
+if(num % i == 0)
+{
+flag = true;
+break;
+}
+}
+if (!flag)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
